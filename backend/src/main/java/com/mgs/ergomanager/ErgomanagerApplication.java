4ed1773bfddfd778fb7dae2d6a8ead057219713c
Brandon <brangonvar@gmail.com>
2026-09-22 14:52:37 -0600

    Agrega estructura inicial del backend con Spring Boot (Task 77)

---
+++ backend/src/main/java/com/mgs/ergomanager/ErgomanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mgs.ergomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErgomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErgomanagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mgs/ergomanager/ErgomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgs.ergomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErgomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
